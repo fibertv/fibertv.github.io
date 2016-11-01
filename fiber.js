9ea075fa7b9b08216179cbda9aef66d771e89423
fibertv <fibermediaproject@gmail.com>
2016-11-01 09:13:27 -0700

    Update fiber.js

--- fiber.js
+++ fiber.js
@@ -3,26 +3,26 @@
                 width: "100%",
                 height: "100%",
                 autoPlay: true,
-                parentId: '#01'
+                parentId: '#ch01'
             });
             var player = new Clappr.Player({
                 source: 'http://abclive.abcnews.com/i/abc_live4@136330/master.m3u8?b=500,300,700,900,1200',
                 width: "100%",
                 height: "100%",
                 autoPlay: true,
-                parentId: '#02'
+                parentId: '#ch02'
             });
             var player = new Clappr.Player({
                 source: 'http://abclive.abcnews.com/i/abc_live4@136330/master.m3u8?b=500,300,700,900,1200',
                 width: "100%",
                 height: "100%",
                 autoPlay: true,
-                parentId: '#03'
+                parentId: '#ch03'
             });
             var player = new Clappr.Player({
                 source: 'http://abclive.abcnews.com/i/abc_live4@136330/master.m3u8?b=500,300,700,900,1200',
                 width: "100%",
                 height: "100%",
                 autoPlay: true,
-                parentId: '#04'
+                parentId: '#ch04'
             });
